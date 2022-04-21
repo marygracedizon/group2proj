1fa1b8c269c864e950109f705a6c22b3b7f809e1
Dizon <iwaydizongrace@gmail.com>
2022-04-21 17:01:20 +0800

    added a homepage

--- index.php
+++ index.php
@@ -5,7 +5,33 @@
 	<title>Sample</title>
 </head>
 <body>
-	<h2>WELCOME TO CEBU
-	</h2>
+	
+<html>
+<head>
+	
+<body bgcolor="Violet">
+<center>
+<br><a href="home.php">INDEX</a>
+<br><h1><font color="black"><font face="Algerian">Tourist Spots in Palawan</h1>
+<img src="images/P1.jpeg" height="250" width="250">
+<img src="images/P2.jpeg" height="250" width="250">
+<img src="images/P3.jpeg" height="250" width="250">
+<img src="images/P4.jpeg" height="250" width="250">
+<img src="images/P5.jpeg" height="250" width="250">
+<img src="images/P6.jpeg" height="250" width="250">
+<img src="images/P7.jpeg" height="250" width="250">
+<img src="images/P8.jpeg" height="250" width="250">
+<img src="images/P9.jpeg" height="250" width="250">
+<img src="images/P10.jpeg" height="250" width="250">
+<img src="images/P11.jpeg" height="250" width="250">
+<img src="images/P12.jpeg" height="250" width="250">
+<img src="images/P13.jpeg" height="250" width="250">
+<img src="images/P14.jpeg" height="250" width="250">
+<img src="images/P15.jpeg" height="250" width="250">
+
+<br><h3><embed src="01 Britt Nicole - Gold.mp3" height"50" width"350">
+</body>
+
+</html>
 </body>
 </html>
